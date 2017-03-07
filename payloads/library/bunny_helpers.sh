e448086819ace7356ec39aec9dc1355f4e1ba220
Carleton Stuberg <carleton@stuberg.com>
2017-03-06 22:59:17 -0500

    Make SWITCH_POSITION usable

--- payloads/library/bunny_helpers.sh
+++ payloads/library/bunny_helpers.sh
@@ -28,15 +28,14 @@ check_switch() {
 	switch3=`cat /sys/class/gpio_sw/PL3/data`
 	echo "--- switch1 = $switch1, switch2 = $switch2, switch3 = $switch3"
 	if [ "x$switch1" = "x0" ] && [ "x$switch2" = "x1" ] && [ "x$switch3" = "x1" ]; then
-		SWITCH_POSITION="switch1"
+		export SWITCH_POSITION="switch1"
 	elif [ "x$switch1" = "x1" ] && [ "x$switch2" = "x0" ] && [ "x$switch3" = "x1" ]; then
-		SWITCH_POSITION="switch2"
+		export SWITCH_POSITION="switch2"
 	elif [ "x$switch1" = "x1" ] && [ "x$switch2" = "x1" ] && [ "x$switch3" = "x0" ]; then
-		SWITCH_POSITION="switch3"
+		export SWITCH_POSITION="switch3"
 	else
-		SWITCH_POSITION="invalid"
+		export SWITCH_POSITION="invalid"
 	fi
 }
 
 check_switch
-export SWITCH_POSITION
