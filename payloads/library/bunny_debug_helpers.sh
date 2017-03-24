27616026ffab001f7dab1d842bd28f8faf5ef46b
bg-wa <bill.j.grow@gmail.com>
2017-03-24 13:25:41 -0700

    Timestamp on debug files

--- payloads/library/bunny_debug_helpers.sh
+++ payloads/library/bunny_debug_helpers.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 ################################################################################
-# Allow Debugging messages written to: "/root/udisk/debug/debug.txt"
+# Allow Debugging messages written to: "/root/udisk/debug/debug_[timestamp].txt"
 # on the BashBunny
 #
 # How this works?
-# 1) Once the library is included in your payload, write text to
+# 1) Once the lobrary is included in your payload, write text to
 #    the debug file with:
 #       echo "DEBUG MESSAGE" >> "${DEBUG_FILE}"
 #	    OR
 #	    debug_log "DEBUG MESSAGE"
 #	    (To write to log with timestamps)
-# 2) After attack, Text can be read at: "/root/udisk/debug/debug.txt"
+# 2) After attack, Text can be read at: "/root/udisk/debug/debug_[timestamp].txt"
 #    on the BashBunny
 # 3) To turn off debugging, pass the OFF comand when including the helper
-#       source bunny_debug_helpers.sh OFF 
+#       source bunny_debug_helpers.sh OFF
 ################################################################################
 
 ################################################################################
@@ -27,11 +27,12 @@ else
 fi
 
 timestamp () {
-    echo "$(date +"%Y-%m-%d_%H-%M-%S"):"
+    echo "$(date +"%Y-%m-%d_%H-%M-%S")"
 }
+ACTIVE_TIME_STAMP="${timestamp}"
 
 start_debug () {
-        DEBUG_FILE="./debug/debug.txt"
+    DEBUG_FILE="./debug/debug_${ACTIVE_TIME_STAMP}.txt"
     if [ ! -d "./debug" ]; then
       mkdir ./debug
     fi
@@ -40,9 +41,8 @@ start_debug () {
 }
 
 debug_log () {
-    echo $(timestamp) "${1}" >> "${DEBUG_FILE}"
+    echo "${timestamp}: ${1}" >> "${DEBUG_FILE}"
 }
-
 
 if [ "${DEBUG_STATE}" = "ON" ]; then
     start_debug
