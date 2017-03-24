a472376f85e2c03f04e8db2ce5f801eca7b65264
bg-wa <bill.j.grow@gmail.com>
2017-03-24 10:04:30 -0700

    Added Timestamps and "debug_log" function
    The debug log function can be called to mark the debug text with a timestamp.
    The debug log function will not fail as gracefully in a script as the 'echo "message" >> ${DEBUG_FILE}', but id does add timestamp functionality.
    
    EXAMPLE
    
    debug_log "DEBUG MESSAGE"
    #outputs > 2017-03-24_09-58-37: DEBUG MESSAGE

--- payloads/library/bunny_debug_helpers.sh
+++ payloads/library/bunny_debug_helpers.sh
@@ -7,6 +7,9 @@
 # How this works?
 # 1) Write text to the debug file with:
 #       echo "DEBUG MESSAGE" >> "${DEBUG_FILE}"
+#	OR
+#	debug_log "DEBUG MESSAGE"
+#	(To write to log with timestamps)
 # 2) After attack, Text can be read at: "/root/udisk/debug/debug.txt"
 # on the BashBunny
 ################################################################################
@@ -20,14 +23,21 @@ else
     DEBUG_STATE="ON"
 fi
 
+timestamp () {
+    echo "$(date +"%Y-%m-%d_%H-%M-%S"):"
+}
 
-start_debug() {
-	DEBUG_FILE="/root/udisk/debug/debug.txt"
-    if [ ! -d "/root/udisk/debug" ]; then
-      mkdir /root/udisk/debug
+start_debug () {
+        DEBUG_FILE="./debug/debug.txt"
+    if [ ! -d "./debug" ]; then
+      mkdir ./debug
     fi
     touch "${DEBUG_FILE}"
-    echo "DEBUG STARTED" >> "${DEBUG_FILE}"
+    echo $(timestamp) "DEBUG STARTED" >> "${DEBUG_FILE}"
+}
+
+debug_log () {
+    echo $(timestamp) "${1}" >> "${DEBUG_FILE}"
 }
 
 
