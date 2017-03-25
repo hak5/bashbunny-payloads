c92f3a5d045e75fa30298d2fea57c4259e10ddc3
TheDragonkeeper <dragonkeeper@nixheads.co.uk>
2017-03-25 00:28:03 +0000

    Update autoipshare.sh

--- payloads/library/Linux-AutoShareIP/autoipshare.sh
+++ payloads/library/Linux-AutoShareIP/autoipshare.sh
@@ -1,6 +1,6 @@
 sbunnynmask=255.255.255.0
 sbunnynet=172.16.64.0/24
-sbunnyhostip=172.16.64.64
+sbunnyhostip=172.16.64.5
 sbunnyip=172.16.64.1
 bunnydetected=$(ip addr | grep '00:11:22' -B1 | awk {'print $2'} | head -1 | grep 'eth\|en')
 hasiproute2=$(which ip)
@@ -26,7 +26,6 @@ do
     ifconfig $sbunnylan $sbunnyhostip netmask $sbunnynmask up &>/dev/null
     sleep 1
 done
-ifconfig $sbunnylan $sbunnyhostip netmask $sbunnynmask up
 echo '1' > /proc/sys/net/ipv4/ip_forward
 iptables -X
 iptables -F
@@ -35,4 +34,3 @@ iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A POSTROUTING -t nat -j MASQUERADE
 route del default
 route add default gw $sbunnygw $sbunnywan
-exit
