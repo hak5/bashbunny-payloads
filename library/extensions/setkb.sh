cbb2b42f1b311b6f397c1d745f6c76a3d8ddd88b
elkentaro <el.kentaro@gmail.com>
2017-04-16 19:42:48 +0900

    Update setkb.sh
    Modified so that its even easier.
    
    SETKB START will set the keyboard to a en-US keyboard layout.
    SETKB DONE will reset the keyboard to the default layout based on the culture settings.
    SET xx-XX will set the keyboard layout to whatever is specified as xx-XX

--- library/extensions/setkb.sh
+++ library/extensions/setkb.sh
@@ -1,37 +1,50 @@
 #!/bin/bash
 #
-# SETKB v1 by @elkentaro
+# SETKB v2.0 by @elkentaro
 # Simplifies executing commands from HID attacks for different language keyboards. on Windows by using Powershell.
 # Usage: SETKB en-US [give the command the 2 letter combination of keyboard settings]
 #
 # Examples:
-# SETKB en-US (set the keyboard layout to a US keyboard layout)
-# SETKB ja-JP (set the keyboard layout to a Japanese 106 layout)
+# SETKB START (set the keyboard layout to a US keyboard layout)
+# SETKB DONE (set the keyboard layout to the default keyboard determined by the OS language settings)
+# SETKB xx-XX (overwrite the keyboard layout to whatever keyboard layout you need, you will need the [lanugage].json file to run Ducky scripts) 
 
 
 function SETKB() {
-   local kb=$1
+   local state=$1
    shift
    
-   [[ -z "$kb" ]] && exit 1 # KB keyboard parameter must be given.
+   [[ -z "$state" ]] && exit 1 # state keyboard parameter must be given.
    
-   case "$kb" in
-      'en-US')
+   case "$state" in
+      'START')
          QUACK GUI r
          QUACK DELAY 500
-         QUACK STRING "powershell.exe Set-WinUserLanguageList -LanguageList en-US -force"
+         QUACK STRING "powershell.exe Set-WinUserLanguageList -LanguageList en-US -force;"
          QUACK ENTER
          QUACK DELAY 1500
 
          ;;
-      *)
+      'DONE')
          QUACK GUI r
          QUACK DELAY 500
-         QUACK "STRING powershell.exe Set-WinUserLanguageList -LanguageList $kb -force"
+         QUACK "STRING powershell.exe \$back2kb=(get-Culture | Select -ExpandProperty Name) ; Set-WinUserLanguageList -LanguageList \$back2kb -force; "
          QUACK ENTER
          QUACK DELAY 1500
 
-         ;;       
+         ;;    
+         
+      *)
+         QUACK GUI r
+         QUACK DELAY 500
+         QUACK "STRING powershell.exe Set-WinUserLanguageList -LanguageList $state -force"
+         QUACK ENTER
+         QUACK DELAY 1500
+
+         ;;    
+
+
+
    esac
 }
 
