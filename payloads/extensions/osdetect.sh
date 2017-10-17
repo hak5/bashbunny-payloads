3bbaf9a63f251e3283ce4136297c09c80b933673
TheDragonkeeper <dragonkeeper@nixheads.co.uk>
2017-10-17 22:01:36 +0100

    removed sleeps
    .

--- payloads/extensions/osdetect.sh
+++ payloads/extensions/osdetect.sh
@@ -8,14 +8,12 @@ function OSDETECT() {
     export TARGET_OS=$OSfound
 }
 ATTACKMODE RNDIS_ETHERNET
-sleep 10
-GET TARGET_IP
 sleep 5
+GET TARGET_IP
 if [ -z $TARGET_IP ]
     ATTACKMODE ECM_ETHERNET
     sleep 5
     GET TARGET_IP
-    sleep 5
 fi
 [[ -z $TARGET_IP ]] && exit
 export -f OSDETECT
