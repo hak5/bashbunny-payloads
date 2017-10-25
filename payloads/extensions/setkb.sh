5a77792c1d2812e91305f177cf17e18780686e12
Aidan Holland <thehappydinoa@gmail.com>
2017-10-24 20:10:17 -0400

    Update and fix payloads (#277)
    * Updated all Payloads for Version 1.2+
    
    Fixed Style Issues on extensions and payloads.
    Added GET TARGET_OS to get.sh
    Removed and Fixed all uses ducky_helper.sh (Issue #248)
    Removed all mention of DUCKY_LANG (Issue #248)
    Renamed Payloads with spaces in name
    Added an extension to keep Macs Happy
    Added a payload for Mac DNS poisoning
    Fixed Issue #271 changed wget to curl -o
    Implemented PR #268
    Implemented PR #273
    
    * Fixed e.cmd
    
    * Fix e.cmd pt2
    
    * Fixed Issues
    
    Fixed issues pointed out by @sebkinne
    Fixed styling errors

--- payloads/extensions/setkb.sh
+++ payloads/extensions/setkb.sh
@@ -7,45 +7,45 @@
 # Examples:
 # SETKB START (set the keyboard layout to a US keyboard layout)
 # SETKB DONE (set the keyboard layout to the default keyboard determined by the OS language settings)
-# SETKB xx-XX (overwrite the keyboard layout to whatever keyboard layout you need, you will need the [lanugage].json file to run Ducky scripts) 
+# SETKB xx-XX (overwrite the keyboard layout to whatever keyboard layout you need, you will need the [lanugage].json file to run Ducky scripts)
 
 
 function SETKB() {
-   local state=$1
-   shift
-   
-   [[ -z "$state" ]] && exit 1 # state keyboard parameter must be given.
-   
-   case "$state" in
-      'START')
-         QUACK GUI r
-         QUACK DELAY 500
-         QUACK STRING "powershell.exe Set-WinUserLanguageList -LanguageList en-US -force;"
-         QUACK ENTER
-         QUACK DELAY 1500
+  local state=$1
+  shift
 
-         ;;
-      'DONE')
-         QUACK GUI r
-         QUACK DELAY 500
-         QUACK "STRING powershell.exe \$sl=(Get-WinSystemLocale | Select -ExpandProperty Name) ; Set-WinUserLanguageList -LanguageList \$sl -force; "
-         QUACK ENTER
-         QUACK DELAY 1500
+  [[ -z "$state" ]] && exit 1 # state keyboard parameter must be given.
 
-         ;;    
-         
-      *)
-         QUACK GUI r
-         QUACK DELAY 500
-         QUACK "STRING powershell.exe Set-WinUserLanguageList -LanguageList $state -force"
-         QUACK ENTER
-         QUACK DELAY 1500
+  case "$state" in
+    'START')
+      QUACK GUI r
+      QUACK DELAY 500
+      QUACK STRING "powershell.exe Set-WinUserLanguageList -LanguageList en-US -force;"
+      QUACK ENTER
+      QUACK DELAY 1500
 
-         ;;    
+      ;;
+    'DONE')
+      QUACK GUI r
+      QUACK DELAY 500
+      QUACK "STRING powershell.exe \$sl=(Get-WinSystemLocale | Select -ExpandProperty Name) ; Set-WinUserLanguageList -LanguageList \$sl -force; "
+      QUACK ENTER
+      QUACK DELAY 1500
+
+      ;;
+
+    *)
+      QUACK GUI r
+      QUACK DELAY 500
+      QUACK "STRING powershell.exe Set-WinUserLanguageList -LanguageList $state -force"
+      QUACK ENTER
+      QUACK DELAY 1500
+
+      ;;
 
 
 
-   esac
+  esac
 }
 
 export -f SETKB
