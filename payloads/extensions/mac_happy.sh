4df763c4a41682d25831d8ee5b93a39d85e68051
Aidan Holland <thehappydinoa@gmail.com>
2018-01-30 17:17:56 -0500

    Update mac_happy extension (#319)

--- payloads/extensions/mac_happy.sh
+++ payloads/extensions/mac_happy.sh
@@ -2,15 +2,28 @@
 
 # Title:     Mac_Happy
 # Author:    thehappydinoa
-# Target:    Mac
-# Version:   0.2
+# Target:    macOS
+# Version:   0.3
 #
 # Makes Mac happy by correctly setting pid and vid
-# Use by running mac_happy ATTACKMODE HID <attack modes here>
+# Use by running MAC_HAPPY HID/ETHERNET/...
 #
 
 function MAC_HAPPY() {
-    [[ "$#" -gt 1 && "$1" == "ATTACKMODE" ]] || exit 1
-    eval "$@ vid_0x05ac pid_0x021e"
+    [[ "$#" -gt 1 ]] || exit 1
+    case "$1" in
+        HID)
+            ATTACKMODE HID vid_0x05ac pid_0x021e
+            ;;
+        ETHERNET)
+            ATTACKMODE ECM_ETHERNET vid_0x05ac pid_0x021e
+            ;;
+        ATTACKMODE)
+            eval "$@ vid_0x05ac pid_0x021e"
+            ;;
+        *)
+            exit 1
+            ;;
+    esac
 }
 export -f MAC_HAPPY
