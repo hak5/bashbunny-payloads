1c79cd56d1f1e8a4433e36dbe623c455adf53bd9
pusherosu <thedosius_dreamtwister@hotmail.com>
2018-01-30 19:18:26 -0400

    Update nyanloader.asm
    Added comments before making pull request

--- payloads/library/prank/Nyanloader/nyanloader.asm
+++ payloads/library/prank/Nyanloader/nyanloader.asm
@@ -1,5 +1,6 @@
 ;nyanloader.asm - replaces sector 0 with custom bootloader intsructions.
-;Copyright 2017 by Pusher. 
+;Renders a single frame directly to video memory
+;Copyright 2018 by Pusher. 
 
 ;This program is free software: you can redistribute it and/or modify
 ;it under the terms of the GNU General Public License as published by
