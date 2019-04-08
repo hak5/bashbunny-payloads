bed7de2cd4eb54ac289b2715b771c49c792b0cae
TheDragonkeeper <dragonkeeper@nixheads.co.uk>
2019-04-08 19:16:09 +0100

    Update payload.sh

--- payloads/extensions/payload.sh
+++ payloads/extensions/payload.sh
@@ -1,3 +1,20 @@
+#!/bin/bash
+
+#Payload selector by Dragonkeeper
+#  Allows selecting payloads by amount of switch changes
+#
+#  Execution RUN_PAYLOAD "/Payload/folder/" "payload1.txt" "anotherpayload.txt" "awesomepayload.txt"
+#
+# LED will go red, to let you know its ready.  It is currently about to execute the 1st given payload
+# the LED will blue, to let you decide if you want to run that payload.  if yes do nothing, if no flick the switch.
+#
+# if you flicked the switch, the LED will flash green to indicate this, it will then flick to red and go blue,  it is now on the 2nd given payload and awaiting decision.
+#
+# if you leave the switch alone while its blue, the LED will go solid green to indicate that the selection is locked in.
+# and the payload of the given number will run.
+#
+# This will let you add as many payloads as you desire.
+
 function RUN_PAYLOAD() {
     payloadcount=$#
     payloadarray=("$@")
